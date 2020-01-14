bb52fcbc16273956d6ada686a967a8526bbab01b
Jason M <jbird206@gmail.com>
2020-01-14 11:02:31 -0800

    Add files via upload

---
+++ webpage-slidedown/js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#flip1").click(function(){
+      $("#panel1").slideDown("slow");
+    });
+
+    $("#flip2").click(function(){
+        $("#panel2").slideDown("slow");
+    });
+    
+    $("#flip3").click(function(){
+        $("#panel3").slideDown("slow");
+    });
+  });
